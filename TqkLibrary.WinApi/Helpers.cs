c609118fdc67ef35594f321bd0ac7864b6fdce31
tqk2811 <tqk2811@gmail.com>
2021-08-09 18:57:02 +0700

    Add Helpers.CaptureWindow

---
+++ TqkLibrary.WinApi/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using PInvoke;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.WinApi
+{
+  public static class Helpers
+  {
+    public static Bitmap CaptureWindow(IntPtr handle)
+    {
+      User32.GetWindowRect(handle, out RECT windowRect);// get the size
+      int width = windowRect.right - windowRect.left;
+      int height = windowRect.bottom - windowRect.top;
+
+      Bitmap bitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+      using Graphics fgx = Graphics.FromImage(bitmap);
+      IntPtr hdc = fgx.GetHdc();
+      User32.PrintWindow(handle, hdc, User32.PrintWindowFlags.PW_FULLWINDOW);
+      fgx.ReleaseHdc(hdc);
+
+      return bitmap;
+    }
+  }
+}
