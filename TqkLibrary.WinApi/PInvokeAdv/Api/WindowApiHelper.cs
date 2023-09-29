df06ed6b596e7f2a1c0813fa8d5460527b1b2cdd
tqk2811 <tqk2811@gmail.com>
2023-09-29 20:13:52 +0700

    fix WindowApiHelper

--- TqkLibrary.WinApi/PInvokeAdv/Api/WindowApiHelper.cs
+++ TqkLibrary.WinApi/PInvokeAdv/Api/WindowApiHelper.cs
@@ -5,21 +5,41 @@ using System.Runtime.InteropServices;
 
 namespace TqkLibrary.WinApi.PInvokeAdv.Api
 {
-  public static class WindowApiHelper
-  {
-    public static IntPtr StructureToPtr<T>(T _struct) where T : struct
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class WindowApiHelper
     {
-      int msgSize = Marshal.SizeOf(_struct);
-      IntPtr ptr = Marshal.AllocCoTaskMem(msgSize);
-      Marshal.StructureToPtr(_struct, ptr, true);
-      return ptr;
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_struct"></param>
+        /// <returns></returns>
+        public static IntPtr StructureToPtr<T>(T _struct) where T : struct
+        {
+            int msgSize = Marshal.SizeOf(_struct);
+            IntPtr ptr = Marshal.AllocCoTaskMem(msgSize);
+            Marshal.StructureToPtr(_struct, ptr, true);
+            return ptr;
+        }
 
-    public static T IntPtrToStructObject<T>(IntPtr intptr) where T : struct
-    {
-      return (T)Marshal.PtrToStructure(intptr, typeof(T));
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="intptr"></param>
+        /// <returns></returns>
+        public static T IntPtrToStructObject<T>(IntPtr intptr) where T : struct
+        {
+            return (T)Marshal.PtrToStructure(intptr, typeof(T));
+        }
 
-    public static Rectangle GetRectangle(this RECT r) => new Rectangle(r.left, r.top, r.right, r.bottom);
-  }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public static Rectangle GetRectangle(this RECT r) => new Rectangle(r.left, r.top, r.right - r.left, r.bottom - r.top);
+    }
 }
