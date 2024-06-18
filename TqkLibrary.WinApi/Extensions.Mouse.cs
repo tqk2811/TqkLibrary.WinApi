72080c2b6a98a44b4cfd6c8177bfa4e047a54582
tqk2811 <tqk2811@gmail.com>
2024-06-18 16:16:12 +0700

    add ControlLSwipeMultiPointAsync

--- TqkLibrary.WinApi/Extensions.Mouse.cs
+++ TqkLibrary.WinApi/Extensions.Mouse.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TqkLibrary.WinApi
 {
@@ -60,6 +63,80 @@ namespace TqkLibrary.WinApi
             {
                 PInvoke.SendMessage((HWND)windowHandle, PInvoke.WM_MOUSEMOVE, new WPARAM(MK_LBUTTON), CreateLParam(to.X, to.Y));
                 PInvoke.SendMessage((HWND)windowHandle, PInvoke.WM_LBUTTONUP, new WPARAM(MK_LBUTTON), CreateLParam(to.X, to.Y));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="windowHandle"></param>
+        /// <param name="points"></param>
+        /// <param name="totalDuration"></param>
+        /// <param name="step"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task ControlLSwipeMultiPointAsync(this IntPtr windowHandle,
+            IReadOnlyList<Point> points,
+            int totalDuration,
+            int step = 10,
+            CancellationToken cancellationToken = default
+            )
+        {
+            if (points.Count < 2)
+                throw new InvalidOperationException($"{nameof(points)}.Count mustbe >= 2");
+            else if (points.Count == 2)
+            {
+                await windowHandle.ControlLSwipeAsync(points.First(), points.Last(), totalDuration, step, cancellationToken);
+                return;
+            }
+
+            double totalRange = 0.0;
+            List<double> ranges = new List<double>();
+            for (int i = 1; i < points.Count; i++)
+            {
+                double range = Math.Sqrt(Math.Pow(points[i - 1].X - points[i].X, 2) + Math.Pow(points[i - 1].Y - points[i].Y, 2));
+                totalRange += range;
+                ranges.Add(range);
+            }
+
+            int times = totalDuration / step;
+            double rangePerStep = totalRange / times;
+
+            List<Point> pointsMove = new List<Point>();
+            for (int i = 1; i < points.Count; i++)
+            {
+                pointsMove.Add(points[i - 1]);
+                double range = ranges[i - 1];
+                int straightTimes = (int)(range / rangePerStep);
+                Point vector = new Point(points[i].X - points[i - 1].X, points[i].Y - points[i - 1].Y);//vector = B - A
+                //cos(vector) = X/rangePerStep => X = cos(vector) * rangePerStep = vector.X / range * rangePerStep
+                //sin(vector) = Y/rangePerStep => Y = sin(vector) * rangePerStep = vector.Y / range * rangePerStep
+                //point = (X,Y) + A
+                for (int j = 0; j < straightTimes; j++)
+                {
+                    pointsMove.Add(new Point(
+                        (int)(vector.X / range * rangePerStep * (j + 1)) + points[i - 1].X,
+                        (int)(vector.Y / range * rangePerStep * (j + 1)) + points[i - 1].Y
+                        ));
+                }
+            }
+            pointsMove.Add(points.Last());
+
+            try
+            {
+                Point first = pointsMove.First();
+                PInvoke.SendMessage((HWND)windowHandle, PInvoke.WM_LBUTTONDOWN, new WPARAM(MK_LBUTTON), CreateLParam(first.X, first.Y));
+                for (int i = 0; i < pointsMove.Count; i++)
+                {
+                    PInvoke.SendMessage((HWND)windowHandle, PInvoke.WM_MOUSEMOVE, new WPARAM(MK_LBUTTON), CreateLParam(pointsMove[i].X, pointsMove[i].Y));
+                    await Task.Delay(step, cancellationToken);
+                }
+            }
+            finally
+            {
+                Point last = pointsMove.Last();
+                PInvoke.SendMessage((HWND)windowHandle, PInvoke.WM_MOUSEMOVE, new WPARAM(MK_LBUTTON), CreateLParam(last.X, last.Y));
+                PInvoke.SendMessage((HWND)windowHandle, PInvoke.WM_LBUTTONUP, new WPARAM(MK_LBUTTON), CreateLParam(last.X, last.Y));
             }
         }
     }
